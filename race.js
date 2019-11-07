7aceae0649357e492a7756c3f20688a9a0575532
Felipe G <pipe2705@gmail.com>
2019-11-07 15:37:25 -0800

    Updated Javascript to show round winners and fixed CSS to show all elements in the correct spaces

--- race.js
+++ race.js
@@ -3,67 +3,76 @@
 let playerOne = {
   totalPoints: 0,
   points: 0,
-  horseName: ' '
-}
+  horseName: " "
+};
 
 let cpu = {
   totalPoints: 0,
   points: 0,
-  horseName: ' '
-}
+  horseName: " "
+};
 
 let addOne = () => {
-  document.body.onkeydown = function (param) {
+  document.body.onkeydown = function(param) {
     if (param.keyCode == 32) {
-      playerOne.points = playerOne.points + Math.floor(Math.random() * 3)
-      document.getElementById('p1points').innerHTML = playerOne.points
-      console.log(playerOne.points)
+      playerOne.points = playerOne.points + Math.floor(Math.random() * 3);
+      document.getElementById("p1points").innerHTML = playerOne.points;
+      console.log(playerOne.points);
 
       // console.log(playerOne.points + " p1 points");
 
-      cpu.points = cpu.points + Math.floor(Math.random() * 3)
-      document.getElementById('cpu-points').innerHTML = cpu.points
-      console.log(cpu.points + ' cpu points')
+      cpu.points = cpu.points + Math.floor(Math.random() * 3);
+      document.getElementById("cpu-points").innerHTML = cpu.points;
+      console.log(cpu.points + " cpu points");
 
-      if (playerOne.points >= 101) {
-        alert('player 1 wins!')
+      if (playerOne.points >= 10) {
+        alert("I can code an alert yeaaaahhhh! Colombia Wins! ");
       }
-      if (cpu.points >= 101) {
-        alert('CPU wins!')
+      if (cpu.points >= 10) {
+        alert("Germany wins! :((((");
       }
-      if ((playerOne.points && cpu.points) === 100) {
-        alert('Its a Tie!')
+      if (playerOne.points >= 10 && cpu.points >= 10) {
+        alert("Its a Tie");
       }
-
-      if (playerOne.points > 100 && playerOne.points > cpu.points) {
-        playerOne.totalPoints = playerOne.totalPoints + 1
-        console.log(playerOne.totalPoints)
-      }
-      if (cpu.points > 100 && cpu.points > playerOne.points) {
-        cpu.totalPoints = cpu.totalPoints + 1
-        console.log(cpu.totalPoints)
-
-        // button next round
-        // reset points and set everything back to 0 except for total points
-        //
-      }
+      roundWinner();
     }
+  };
+};
+
+let roundWinner = () => {
+  if (playerOne.points > 9.99 && playerOne.points > cpu.points) {
+    playerOne.totalPoints = playerOne.totalPoints + 1;
+    document.getElementById("roundsWonp1").innerText = playerOne.totalPoints;
   }
-}
+  if (cpu.points > 9.99 && cpu.points > playerOne.points) {
+    cpu.totalPoints = cpu.totalPoints + 1;
+    document.getElementById("roundsWoncpu").innerHTML = cpu.totalPoints;
+    console.log(cpu.totalPoints);
+  }
+};
+
+let nextRound;
+let nextRoundButton = document.querySelector(".round2");
+nextRoundButton.addEventListener(
+  "click",
+  (nextRound = () => {
+    playerOne.points = 0;
+    cpu.points = 0;
+    document.getElementById("p1points").innerHTML = playerOne.points;
+    document.getElementById("cpu-points").innerHTML = cpu.points;
+  })
+);
 
 let progressBar = () => {
-  document.getElementById('player1Bar').value = playerOne.points
-  document.getElementById('cpuBar').value = cpu.points
-}
+  document.getElementById("player1Bar").value = playerOne.points;
+  document.getElementById("cpuBar").value = cpu.points;
+};
 
 let refreshPage = () => {
-  window.location.reload()
-}
+  window.location.reload();
+};
 
-document.addEventListener('keypress', addOne)
-document.addEventListener('keypress', progressBar)
-progressBar()
-console.log(cpu)
-
-//  document.addEventListener("keypress", gameEnd())
-// console.log(playerOne.points)
+document.addEventListener("keypress", addOne);
+document.addEventListener("keypress", progressBar);
+progressBar();
+console.log(cpu);
