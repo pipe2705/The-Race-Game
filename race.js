64127fae916d52676736c45d8aa3d00791acb815
Felipe G <pipe2705@gmail.com>
2019-11-07 19:00:44 -0800

    Made changes to the instructions so they dissapear after the first round starts...

--- race.js
+++ race.js
@@ -20,31 +20,40 @@ let addOne = () => {
       console.log(playerOne.points);
 
       // console.log(playerOne.points + " p1 points");
-
       cpu.points = cpu.points + Math.floor(Math.random() * 3);
       document.getElementById("cpu-points").innerHTML = cpu.points;
       console.log(cpu.points + " cpu points");
 
-      if (playerOne.points >= 10) {
+      if (playerOne.points >= 50) {
         alert("I can code an alert yeaaaahhhh! Colombia Wins! ");
       }
-      if (cpu.points >= 10) {
+      if (cpu.points >= 50) {
         alert("Germany wins! :((((");
       }
-      if (playerOne.points >= 10 && cpu.points >= 10) {
+      if (playerOne.points >= 50 && cpu.points >= 50) {
         alert("Its a Tie");
       }
       roundWinner();
+      gameWinnerCheck();
     }
   };
 };
 
+let gameWinnerCheck = () => {
+  if (playerOne.totalPoints >= 2) {
+    alert("Player 1 Wins Game End of Rounds");
+  }
+  if (cpu.totalPoints >= 2) {
+    alert("CPU Wins the Game End of Rounds");
+  }
+};
+
 let roundWinner = () => {
-  if (playerOne.points > 9.99 && playerOne.points > cpu.points) {
+  if (playerOne.points > 49 && playerOne.points > cpu.points) {
     playerOne.totalPoints = playerOne.totalPoints + 1;
     document.getElementById("roundsWonp1").innerText = playerOne.totalPoints;
   }
-  if (cpu.points > 9.99 && cpu.points > playerOne.points) {
+  if (cpu.points > 49 && cpu.points > playerOne.points) {
     cpu.totalPoints = cpu.totalPoints + 1;
     document.getElementById("roundsWoncpu").innerHTML = cpu.totalPoints;
     console.log(cpu.totalPoints);
@@ -64,6 +73,7 @@ nextRoundButton.addEventListener(
 );
 
 let progressBar = () => {
+  document.getElementById("start-window").textContent = "HDJSGADHJG";
   document.getElementById("player1Bar").value = playerOne.points;
   document.getElementById("cpuBar").value = cpu.points;
 };
@@ -74,5 +84,5 @@ let refreshPage = () => {
 
 document.addEventListener("keypress", addOne);
 document.addEventListener("keypress", progressBar);
-progressBar();
-console.log(cpu);
+
+// progressBar();
